d88b459d4fb8db145b7afe969d532e9dfeee3a9e
Lynn5148 <sagar.rajput5158@gmail.com>
2026-04-03 02:30:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 36227456
+API_HASH = "e8cac19e5ebfaba297f75db50d221fa9"
+BOT_TOKEN = "your_bot_token"
+
+FORCE_CHANNEL = -100XXXXXXXXXX  # your channel id
+ADMINS = [your_user_id]
+DAILY_LIMIT = 10
