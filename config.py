5e61af38bd6c62ca4d95dd3c400fc0c483c995e2
Lynn5148 <sagar.rajput5158@gmail.com>
2026-04-03 02:42:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FORCE_CHANNEL = -100XXXXXXXXXX  # your channel ID
-ADMINS = [123456789]  # your Telegram user ID
+FORCE_CHANNEL = -1003371628062 # your channel ID
+ADMINS = [1893428612]  # your Telegram user ID
 DAILY_LIMIT = 10
