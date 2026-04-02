267468ee35b2d8e9bb387f8dcc66335eb315d82f
Lynn5148 <sagar.rajput5158@gmail.com>
2026-04-03 03:39:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,47 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FORCE_CHANNEL = -1003371628062 # your channel ID
-ADMINS = [1893428612]  # your Telegram user ID
-DAILY_LIMIT = 10
+# 🔁 Interval in seconds (4 hours)
+INTERVAL = 4 * 60 * 60
+
+# 🔥 MODES
+MODES = {
+    "onlyfan": {
+        "channels": [-1001111111111],
+        "caption": """🌸 #{name}
+leaked 💠 vip
+
+⬇️ Download file
+{link}
+{link}
+{link}
+                   
+#{name}
+
+#OnlyFans 
+@HeavenFallNetwork 🔞""",
+        "sticker": "STICKER_ID_1"
+    },
+
+    "adult": {
+        "channels": [
+            -1002222222222,
+            -1003333333333,
+            -1004444444444
+        ],
+        "caption": """👤 {name}
+🎥 {company}
+
+🗓 2025 🔥
+
+#HardCore #adult
+
+🔗 Download media
+{link}
+{link}
+{link}
+
+@heavenfallnetwork""",
+        "sticker": "STICKER_ID_2"
+    }
+}
