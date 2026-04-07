b479078c6db6c1caac5000bfd7da3482d54dfb61
Lynn5148 <sagar.rajput5158@gmail.com>
2026-04-08 02:36:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,34 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMINS = [1893428612, 2117537421]
 
 # 📢 Sticker ID jo har post ke baad jayega
-STICKER_ID = "CAACAgUAAxkBAAEDFxhp066Jt1ctY6DWotJo-dtv1Y4CKAAC0RoAAmSv8FfeFY0gJCkIIDsE" # <-- Yahan apna Sticker File ID dalo
+STICKER_ID = "CAACAgUAAxkBAAEDFxhp066Jt1ctY6DWotJo-dtv1Y4CKAAC0RoAAmSv8FfeFY0gJCkIIDsE"
 
-# 📡 Target Channels
+# 📡 All Target Channels
 CHANNELS = {
-    "1": {"name": "OnlyFans Main", "id": -1003358300432},
-    "2": {"name": "Adult Main 1", "id": -1003624513206},
-    "3": {"name": "Adult Main 2", "id": -1003339318325}
+    "1": {"name": "OnlyFans", "id": -1003358300432},
+    "2": {"name": "CornHub", "id": -1003624513206},
+    "3": {"name": "International Corn", "id": -1003339318325},
+    "4": {"name": "Milfy", "id": -1003668604630},
+    "5": {"name": "Indian", "id": -1003840146959},
+    "6": {"name": "Cornhwa", "id": -1003376886471},
+    "7": {"name": "Doujinshi", "id": -1003853835138}
 }
 
 # 🔥 Caption Templates
 MODES = {
     "onlyfans": {
-        "caption": """🌸 {name}\n\n💠 Exclusive VIP Content\nCarefully curated premium drop\n\n━━━━━━━━━━━━━━\nTap the button below to access\n━━━━━━━━━━━━━━\n\n#OnlyFans\n@HeavenFallNetwork 🔞"""
+        "caption": "🌸 {name}\n\n💠 Exclusive VIP Content\nCarefully curated premium drop\n\n━━━━━━━━━━━━━━\nTap the button below to access\n━━━━━━━━━━━━━━\n\n#OnlyFans\n@HeavenFallNetwork 🔞"
     },
     "adult": {
-        "caption": """🎬 {company}\n\n👤 Featuring: {name}\n\n━━━━━━━━━━━━━━\n💠 Premium Release\nAccess via button below\n━━━━━━━━━━━━━━\n\n#Adult\n@HeavenFallNetwork 🔞"""
+        "caption": "🎬 {company}\n\n👤 Featuring: {name}\n\n━━━━━━━━━━━━━━\n💠 Premium Release\nAccess via button below\n━━━━━━━━━━━━━━\n\n#Adult\n@HeavenFallNetwork 🔞"
+    },
+    "indian": {
+        "caption": "{description} 😋 🔥\n\n➪ Videos: {duration} min Duration\n➪ Rating: 100/10\n\n💦💦 Must watch👁👅👁\nHighly Recommended Stuffs 🐶🥵\n\n@HeavenFallNetwork 🔞"
+    },
+    "cornhwa": {
+        "caption": "**{name} | HeavenFall Cornhwa |**\n\n┏━━━━━━━━━━━━━━━┓\n\n‣ Type : CORNHWA\n‣ Average Rating : 99\n‣ Status : {status}\n‣ No of chapters : {chapters}\n‣ Genres : Drama, Hentai, Romance\n\n┗━━━━━━━━━━━━━━━┛\n**❖ CLICK ON READ NOW BUTTON ❖**\n▬▬▬▬▬▬▬▬▬▬▬▬▬\n﻿\nProvided by @HeavenFallNetwork\n\n[#MustRead #recommended]"
+    },
+    "doujinshi": {
+        "caption": "**{name} | HeavenFallNetwork |**\n\n» Nᴏ.ᴏғ Pᴀɢᴇs: {pages}\n» Tʏᴘᴇ: #doujinshi\n» Lᴀɴɢᴜᴀɢᴇ: #english \n» Tᴀɢs: #heavenfallnetwork #mature #adult #doujinshi \n\n═════════════════════\nProvided by @HeavenFallNetwork\n═════════════════════"
     }
 }
