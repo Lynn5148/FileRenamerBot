1126f689d233a8495462d618f4608a79babe962f
Lynn5148 <sagar.rajput5158@gmail.com>
2026-04-18 22:41:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMINS = [1893428612, 2117537421]
+ADMINS = [1893428612,2117537421,8607152118]
 
 # 📢 Sticker ID jo har post ke baad jayega
 STICKER_ID = "CAACAgUAAxkBAAEDFxhp066Jt1ctY6DWotJo-dtv1Y4CKAAC0RoAAmSv8FfeFY0gJCkIIDsE"
